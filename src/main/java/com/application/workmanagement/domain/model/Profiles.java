29f4e5a73e7b5566b38841b8feb86a39274632d9
Shakthichaithanya <shakthits01@gmail.com>
2023-03-01 11:22:14 +0530

    application properties

--- src/main/java/com/application/workmanagement/domain/model/Profiles.java
+++ src/main/java/com/application/workmanagement/domain/model/Profiles.java
@@ -1,6 +1,8 @@
 package com.application.workmanagement.domain.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class Profiles {
 
 	@Id
-
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
 	private String name;
